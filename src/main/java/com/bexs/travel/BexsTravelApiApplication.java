9c6d31e5254d4bc45c9249759ab97d78086426d4
Luiz Barros <luiz.barros@seenbytech.com>
2020-06-06 14:13:06 -0300

    Initial commit

---
+++ src/main/java/com/bexs/travel/BexsTravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.bexs.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BexsTravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BexsTravelApiApplication.class, args);
+	}
+
+}
